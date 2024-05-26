7f9254c7c8890fd6eeb65dd98247088907b73287
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2024-05-26 15:07:26 +0530

    add serverless function

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,12 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-
+import { sayHello } from './functions/send-journal-reminder/resource';
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 defineBackend({
   auth,
   data,
+  sayHello
 });
